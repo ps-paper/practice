e8b5f75a3cc01049e72914fdd9bd37ae14750da8
ps-paper <rillysun2@handong.ac.kr>
2023-09-06 23:22:20 +0900

    add project1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {s
 
         new WordManager().start();
     }
